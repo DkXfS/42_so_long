403503d0b68a26ad2e037a54739390a8b498d431
AntonioP <thebomb.mg@gmail.com>
2025-09-09 17:52:41 +0200

    Fixed wall collission

--- movement.c
+++ movement.c
@@ -10,22 +10,22 @@ void update_character_position(struct animation *anim, struct key_states *keys, 
 
     if (keys->up)
     {
-        if(mapModY >= MOVE_SPEED || (mapY > 0 && (map[mapY - 1][mapX] != '1' && map[mapY - 1][mapX + 1] != '1')))
+        if(mapModY >= MOVE_SPEED || (map[mapY - 1][mapX] != '1' && map[mapY - 1][mapX + (mapModX > 0)] != '1'))
             anim->characters[0].y -= MOVE_SPEED;
     }
     if (keys->down)
     {
-        if((tileWH - mapModY >= MOVE_SPEED) || map[mapY + 1][mapX] != '1')
+        if(map[mapY + 1][mapX] != '1' && map[mapY + 1][mapX + (mapModX > 0)] != '1')
             anim->characters[0].y += MOVE_SPEED;
     }
     if (keys->left)
     {
-        if(mapModX >= MOVE_SPEED || (mapX > 0 && map[mapY][mapX - 1] != '1'))
+        if(mapModX >= MOVE_SPEED || (map[mapY][mapX - 1] != '1' && map[mapY + (mapModY > 0)][mapX - 1] != '1'))
             anim->characters[0].x -= MOVE_SPEED;
     }
     if (keys->right)
     {
-        if(mapModX <= (tileWH - MOVE_SPEED) || map[mapY][mapX + 2] != '1')
+        if(map[mapY][mapX + 1] != '1' && map[mapY + (mapModY > 0)][mapX + 1] != '1')
             anim->characters[0].x += MOVE_SPEED;
     }
 
